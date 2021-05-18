4c062f027766f55cb5c01449c9e465df01fc4657
luis-luciqm <redbullfc18@gmail.com>
2021-05-17 23:26:36 -0300

    criando templates e adicionando elementos  bootstrap

--- app.js
+++ app.js
@@ -2,11 +2,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-
 const admin = require('./routes/admin') // importando arquivo de rotas de maneira correta
-
 // const mongoose = require('mongoose')
 const app = express()
+const path = require('path') // importando para configurar pasta public, path serve para manipular pastas
 
 // Configurações
 // configurando body-parser
@@ -16,6 +15,12 @@ app.use(bodyParser.json()) // em caso de erro, ver link salvo no chrome
 // configurando handlebars
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+// Mongoose
+
+// Public
+app.use(express.static(path.join(__dirname, "public")))
+// a pasta que está guardando todos os arquivos estaticos é a pasta public
 
 // Rotas
 // obs: rotas sempre abaixo das configurações
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send("Blog - App")
+    res.render("admin/index")
 })
 
 router.get('/posts', (req, res) => {
