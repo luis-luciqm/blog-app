c540c30ce588dceceaa326f0730cf4779a873287
luis-luciqm <redbullfc18@gmail.com>
2021-05-17 22:38:56 -0300

    separando rotas em grupo

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+
+const admin = require('./routes/admin') // importando arquivo de rotas de maneira correta
+
 // const mongoose = require('mongoose')
 const app = express()
 
 // Configurações
-
 // configurando body-parser
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json()) // em caso de erro, ver link salvo no chrome
@@ -16,9 +18,12 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 // Rotas
-app.get('/', (req, res) => {
-    res.send('Blog-APP')
-})
+// obs: rotas sempre abaixo das configurações
+app.use('/admin', admin)
+// prefixo '/admin'
+// Ex: localhost:prefixo/rota
+// localhost:admin/posts
+
 
 // Outros
 const PORT = 5000
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+// rotas destinadas para pagina admin, grupo de rotas
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send("Blog - App")
+})
+
+router.get('/posts', (req, res) => {
+    res.send("Pagina de posts")
+})
+
+router.get('/cadCategorias', (req, res) => {
+    res.send("Pagina de categorias")
+})
+
+module.exports = router // exportando router
